bd860e6b698fdc39340474d08a0a0b7e5176e9c1
lanxsystem <lanxsystem@gmail.com>
2020-12-19 22:24:50 -0500

    update component from class to function...add css to link and fix eslint

--- src/components/Site/AuthModal.js
+++ src/components/Site/AuthModal.js
@@ -1,44 +1,68 @@
-function AuthModal(){
-    return(
-        <div>
-                      <div style={{opacity: 1}} className="af-class-app-auth-modal af-class-signup-modal">
-              <div className="af-class-app-auth-div">
-                <div data-w-id="53c7b13b-a011-0f3f-c933-54abd5353e1a" className="af-class-auth-close"></div>
-                <div className="af-class-app-authheader">Register for an Account</div>
-                <div className="af-class-app-auth-sub">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</div>
-                <div className="w-form">
-                  <form id="email-form-2" name="email-form-2" data-name="Email Form 2"><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="Full-Name" data-name="Full Name" placeholder="Full Name" id="Full-Name" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="email" data-name="email" placeholder="Email Address" id="email" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="phone" data-name="phone" placeholder="Phone Number" id="phone" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="password" data-name="password" placeholder="Password" id="password" />
-                    <a href="#" className="af-class-app-button w-button">Get Started</a>
-                  </form>
-                  <div className="w-form-done">
-                    <div>Thank you! Your submission has been received!</div>
-                  </div>
-                  <div className="w-form-fail">
-                    <div>Oops! Something went wrong while submitting the form.</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div style={{opacity: 1}} className="af-class-app-auth-modal af-class-login-modal">
-              <div className="af-class-app-auth-div">
-                <div data-w-id="d438c752-840a-1615-6219-0dc04b857a2e" className="af-class-auth-close af-class-signup"></div>
-                <div className="af-class-app-authheader">Login into your account</div>
-                <div className="af-class-app-auth-sub">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</div>
-                <div className="w-form">
-                  <form id="email-form-2" name="email-form-2" data-name="Email Form 2"><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="email-2" data-name="Email 2" placeholder="Email Address" id="email-2" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="password-2" data-name="Password 2" placeholder="Password" id="password-2" />
-                    <a href="home-auth.html" className="af-class-app-button w-button">Login into account</a>
-                  </form>
-                  <div className="w-form-done">
-                    <div>Thank you! Your submission has been received!</div>
-                  </div>
-                  <div className="w-form-fail">
-                    <div>Oops! Something went wrong while submitting the form.</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
-    )
+import PropTypes from 'prop-types';
+
+function Login() {
+	return (
+		<>
+			<div className="af-class-app-authheader">Login into your account</div>
+			<div className="af-class-app-auth-sub">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</div>
+			<div className="w-form">
+				<form id="email-form-2" name="email-form-2" data-name="Email Form 2"><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="email-2" data-name="Email 2" placeholder="Email Address" id="email-2" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="password-2" data-name="Password 2" placeholder="Password" id="password-2" />
+					<a href="home-auth.html" className="af-class-app-button w-button">Login into account</a>
+				</form>
+				<div className="w-form-done">
+					<div>Thank you! Your submission has been received!</div>
+				</div>
+				<div className="w-form-fail">
+					<div>Oops! Something went wrong while submitting the form.</div>
+				</div>
+			</div>
+		</>
+	);
 }
+
+function Register() {
+	return (
+		<>
+			<div className="af-class-app-authheader">Register for an Account</div>
+			<div className="af-class-app-auth-sub">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</div>
+			<div className="w-form">
+				<form id="email-form-2" name="email-form-2" data-name="Email Form 2"><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="Full-Name" data-name="Full Name" placeholder="Full Name" id="Full-Name" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="email" data-name="email" placeholder="Email Address" id="email" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="phone" data-name="phone" placeholder="Phone Number" id="phone" /><input type="text" className="af-class-app-auth-textfield w-input" maxLength={256} name="password" data-name="password" placeholder="Password" id="password" />
+					<a href="#" className="af-class-app-button w-button">Get Started</a>
+				</form>
+				<div className="w-form-done">
+					<div>Thank you! Your submission has been received!</div>
+				</div>
+				<div className="w-form-fail">
+					<div>Oops! Something went wrong while submitting the form.</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+function AuthModal(props) {
+	let content;
+	const modalClassList = props.auth.show ? "af-class-app-auth-modal af-class-app-auth-show-modal" : "af-class-app-auth-modal af-class-app-auth-hide-modal";
+	if(props.auth.context === 'login')
+		content = <Login />;
+	else if(props.auth.context === 'register')
+		content =  <Register />;
+
+	return (
+		<div>
+			<div className={modalClassList}>
+				<div className="af-class-app-auth-div">
+					<div onClick={() => { props.handleShow({ show: false, context: '' }); }} className="af-class-auth-close"></div>
+					{content}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+AuthModal.propTypes = {
+	auth: PropTypes.object,
+	handleShow:PropTypes.func
+};
 
 export default AuthModal;
--- src/views/helpers.js
+++ src/views/helpers.js
@@ -1,48 +1,44 @@
-/* eslint-disable */
-
-const React = require('react')
+const React = require('react');
 
 exports.transformProxies = (children = []) => {
-  children = [].concat(children).filter(Boolean)
+	children = [].concat(children).filter(Boolean);
 
-  const proxies = {}
+	const proxies = {};
 
-  React.Children.forEach(children, (child) => {
-    const props = Object.assign({}, child.props)
+	React.Children.forEach(children, (child) => {
+		const props = Object.assign({}, child.props);
 
-    if (!proxies[child.type]) {
-      proxies[child.type] = props
-    }
-    else if (!(proxies[child.type] instanceof Array)) {
-      proxies[child.type] = [proxies[child.type], props]
-    }
-    else {
-      proxies[child.type].push(props)
-    }
+		if (!proxies[child.type]) {
+			proxies[child.type] = props;
+		}
+		else if (!(proxies[child.type] instanceof Array)) {
+			proxies[child.type] = [proxies[child.type], props];
+		}
+		else {
+			proxies[child.type].push(props);
+		}
 
-    if (child.key != null) {
-      props.key = child.key
-    }
+		if (child.key !== null) {
+			props.key = child.key;
+		}
 
-    if (child.ref != null) {
-      props.ref = child.ref
-    }
-  })
+		if (child.ref !== null) {
+			props.ref = child.ref;
+		}
+	});
 
-  return proxies
-}
+	return proxies;
+};
 
 exports.createScope = (children, callback) => {
-  const proxies = exports.transformProxies(children)
+	const proxies = exports.transformProxies(children);
 
-  return callback(proxies)
-}
+	return callback(proxies);
+};
 
 exports.map = (props, callback) => {
-  if (props == null) return null
-  if (!(props instanceof Array)) return callback(props)
+	if (props === null) return null;
+	if (!(props instanceof Array)) return callback(props);
 
-  return props.map(callback)
-}
-
-/* eslint-enable */
+	return props.map(callback);
+};
